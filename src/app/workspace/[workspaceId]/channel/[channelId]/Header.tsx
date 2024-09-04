7db3394126e60e6937677feb1b7a433d75a7678d
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-09-04 16:00:55 +0600

    Chanel header Component Done

--- src/app/workspace/[workspaceId]/channel/[channelId]/Header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/Header.tsx
@@ -1,22 +1,101 @@
-import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { TrashIcon } from "lucide-react";
 import { FaChevronDown } from "react-icons/fa";
 
+import { useUpdateChannel } from "@/features/channels/api/use-update-channel";
+import { useRemoveChannel } from "@/features/channels/api/use-remove-channel";
+
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useConfirm } from "@/hooks/use-confirm";
+
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { TrashIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
 
 interface HeaderProps {
   title: string;
 }
 
 export const Header = ({ title }: HeaderProps) => {
+  const router = useRouter();
+  const channelId = useChannelId();
+  const workspaceId = useWorkspaceId();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete this channel?",
+    "You are about to delete this channel. This action is irreversible",
+  );
+
+  const [value, setValue] = useState(title);
+  const [editOpen, setEditOpen] = useState(false);
+
+  const { data: member } = useCurrentMember({ workspaceId });
+  const { mutate: updateChannel, isPending: isUpdatingChannel } =
+    useUpdateChannel();
+  const { mutate: removeChannel, isPending: isRemovingChannel } =
+    useRemoveChannel();
+
+  const handleEditOpen = (value: boolean) => {
+    if (member?.role !== "admin") return;
+    setEditOpen(true);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLocaleLowerCase();
+    setValue(value);
+  };
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    removeChannel(
+      { id: channelId },
+      {
+        onSuccess: () => {
+          toast.success("Channe deleted");
+          router.push(`/workspace/${workspaceId}`);
+        },
+        onError: () => {
+          toast.error("Failed to delete channel");
+        },
+      },
+    );
+  };
+
+  const handleSudmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    updateChannel(
+      { id: channelId, name: value },
+      {
+        onSuccess: () => {
+          toast.success("Channel update");
+          setEditOpen(false);
+        },
+        onError: () => {
+          toast.error("Faild to update channel");
+        },
+      },
+    );
+  };
+
   return (
     <div className="flex h-[49px] items-center overflow-hidden border bg-white px-4">
+      <ConfirmDialog />
+
       <Dialog>
         <DialogTrigger asChild>
           <Button
@@ -30,26 +109,65 @@ export const Header = ({ title }: HeaderProps) => {
         </DialogTrigger>
 
         <DialogContent className="overflow-hidden bg-gray-50 p-0">
-          <DialogHeader>
+          <DialogHeader className="border-b bg-white p-4">
             <DialogTitle># {title}</DialogTitle>
           </DialogHeader>
 
           <div className="flex flex-col gap-y-2 px-4 pb-4">
-            <div className="cursor-pointer rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
-              <div className="flex items-center justify-between">
-                <p className="text-sm font-semibold">Channel name</p>
-                <p className="text-sm font-semibold text-[#1264a3] hover:underline">
-                  Edit
-                </p>
-              </div>
+            <Dialog open={editOpen} onOpenChange={handleEditOpen}>
+              <DialogTrigger asChild>
+                <div className="cursor-pointer rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Channel name</p>
+                    {member?.role === "admin" && (
+                      <p className="text-sm font-semibold text-[#1264a3] hover:underline">
+                        Edit
+                      </p>
+                    )}
+                  </div>
 
-              <p className="text-sm"> # {title} </p>
-            </div>
+                  <p className="text-sm"> # {title} </p>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Rename this channel</DialogTitle>
+                </DialogHeader>
 
-            <button>
-              <TrashIcon className="size-4" />
-              <p className="text-sm font-semibold"> Delete channel </p>
-            </button>
+                <form onSubmit={handleSudmit} className="space-y-4">
+                  <Input
+                    value={value}
+                    disabled={isUpdatingChannel}
+                    onChange={handleChange}
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                    placeholder="e.g. plan-budget"
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline" disabled={isUpdatingChannel}>
+                        Cancel
+                      </Button>
+                    </DialogClose>
+
+                    <Button disabled={isUpdatingChannel}>Save</Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+
+            {member?.role === "admin" && (
+              <button
+                onClick={handleDelete}
+                disabled={isRemovingChannel}
+                className="flex cursor-pointer items-center gap-x-2 rounded-lg border bg-white px-5 py-4 text-rose-600 hover:bg-gray-50"
+              >
+                <TrashIcon className="size-4" />
+                <p className="text-sm font-semibold"> Delete channel </p>
+              </button>
+            )}
           </div>
         </DialogContent>
       </Dialog>
---
+++ src/features/channels/api/use-remove-channel.ts
@@ -0,0 +1,60 @@
+import { useCallback, useMemo, useState } from "react";
+import { useMutation } from "convex/react";
+
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = { id: Id<"channels"> };
+type ResponseType = Id<"channels"> | null;
+
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+export const useRemoveChannel = () => {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.channels.remove);
+
+  const mutate = useCallback(
+    async (values: RequestType, Options?: Options) => {
+      try {
+        setData(null);
+        setError(null);
+        setStatus("pending");
+
+        const response = await mutation(values);
+        Options?.onSuccess?.(response);
+
+        return response;
+      } catch (error) {
+        setStatus("error");
+
+        Options?.onError?.(error as Error);
+
+        if (Options?.throwError) {
+          throw error;
+        }
+      } finally {
+        setStatus("settled");
+        Options?.onSettled?.();
+      }
+    },
+    [mutation],
+  );
+
+  return { mutate, data, error, isError, isPending, isSettled, isSuccess };
+};
